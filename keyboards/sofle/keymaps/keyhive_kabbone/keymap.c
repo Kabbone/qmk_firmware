db168676894c7c9c71fa84387aabf6b298658ee2
Kabbone <tobias@opel-online.de>
2022-10-29 15:56:25 +0200

    remove ESC from SYM

--- keyboards/sofle/keymaps/keyhive_kabbone/keymap.c
+++ keyboards/sofle/keymaps/keyhive_kabbone/keymap.c
@@ -239,7 +239,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   QK_GESC, KC_Q  , KC_W  , KC_F  , KC_P  , KC_B ,                       KC_J , KC_L  , KC_U   , KC_Y  , KC_QUOT, KC_BSLS,
   KC_TAB , HOME_A, HOME_R, HOME_S, HOME_T, KC_G ,                       KC_M , HOME_N, HOME_E , HOME_I, HOME_O , KC_SCLN,
   ADJ_GUI, KC_Z  , KC_X  , KC_C  , KC_D  , KC_V , KC_MUTE,       KC_NO, KC_K , KC_H  , KC_COMM, KC_DOT, KC_SLSH, KC_EQL,
-                  KC_LCTRL,KC_LALT,SYM_ESC,KC_LSFT,KC_ENT,       KC_BSPC,KC_SPC, NAV_DEL, KC_LBRC, KC_RBRC
+                  KC_LCTRL,KC_LALT,MO(_SYM),KC_LSFT,KC_ENT,       KC_BSPC,KC_SPC, NAV_DEL, KC_LBRC, KC_RBRC
 ),
 
 /* SYM
